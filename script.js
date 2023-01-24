ada78e50e386dd0f958abe4937a0689cf5568cd2
Durga0808 <117895509+Durga0808@users.noreply.github.com>
2023-01-24 22:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+let check=str.toLowerCase();
+	var start=0;var end=check.length()-1;
+	while(start<end){
+		if(check.charAt(start)!=check.charAt(end)){
+			return false;
+		}else{
+			start++;
+			end--;
+		}
+	}
+	return true;
+	
 }
 module.exports = palindrome
